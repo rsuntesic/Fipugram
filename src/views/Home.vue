7fa3617512a945cacc17f8031ac27d977815018f
Your Name <rudolf.suntesic@gmail.com>
2020-12-22 18:13:30 +0100

    vjezba#3-part1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,13 @@
 <template>
  <div class="row">
- <div class="col-2"></div>
- <div class="col-7">
-<InstagramCard/>
-<InstagramCard/>
-<InstagramCard/>
-<InstagramCard/>
-<InstagramCard/>
-<InstagramCard/>
-<InstagramCard/>
-<InstagramCard/>
- </div>
- <div class="col-3">
- ... ovdje ide desni stupac!
+ <div class="col-8">
+<InstagramCard v-for="card in cards" :key="card" :info="card"/>      <!--direktiva vue for petlja(vrijednosti koje ce se ponavljati)-->
+ <!--key atribut koji je u js, mora ici :key ako je v-fpor petlja-->
+
+</div>
+ <div class="col-4">
+
+
  </div>
  </div>
 </template>
@@ -21,8 +16,28 @@
 // @ is an alias to /src
 import InstagramCard from '@/components/InstagramCard.vue'
 
-export default {
+let cards = []
+
+//...API/firebase -> sve kartice -> cards
+
+cards = [
+
+"https://picsum.photos/id/1/400/400",
+"https://picsum.photos/id/2/400/400",
+"https://picsum.photos/id/3/400/400"
+
+]
+
+
+
+export default {                //uputa za vue.js kako oblikovati template(tu komponentnu)
   name: 'Home',
+  data: function(){
+    return{
+      //..Kljucevi vrijednosti
+      cards: cards,
+    }
+  },
   components: {
     InstagramCard,
   }
